9a3082512f81b967e76f84d894228073eb61d4f2
Sankar <433115@cognizant.com>
2021-05-30 17:29:31 +0530

    first commit

---
+++ src/main/java/com/ready/SteadyGo/SteadyGoApplication.java
@@ -0,0 +1,13 @@
+package com.ready.SteadyGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  SteadyGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteadyGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ready/SteadyGo/SteadyGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ready.SteadyGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteadyGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
